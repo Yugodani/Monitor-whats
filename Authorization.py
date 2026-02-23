35c3a630fbc03f5e2441ccf847e50b11ea96c18a
Yugodani <103206796+Yugodani@users.noreply.github.com>
2026-02-23 15:23:02 -0300

    Create Authorization.py

---
+++ Authorization.py
@@ -0,0 +1,15 @@
+import requests
+username = 'Thiagogyn2026'
+token = 'e73626968239ef8aacb22b2f7ecb345f79c895ec'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
