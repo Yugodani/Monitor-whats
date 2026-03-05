a298384650c4cbe3496f0dee59eff7b4fe479603
Yugodani <thiago.olimpio.oliveira@gmail.com>
2026-03-05 16:07:27 -0300

    Remove endpoints de diagnóstico e configura produção

--- apps/calls/Admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Call
-
-class CallAdmin(admin.ModelAdmin):
-    list_display = ['phone_number', 'call_type', 'call_date']
-
-admin.site.register(Call, CallAdmin)  # ← Registro explícito
