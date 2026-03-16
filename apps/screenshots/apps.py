2e3dfe2de1d9f2ac05798e653780cc2cf4e4bea3
Yugodani <thiago.olimpio.oliveira@gmail.com>
2026-03-16 16:26:21 -0300

    Alterações na screenshot

--- apps/screenshots/apps.py
+++ apps/screenshots/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class ScreenshotsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'screenshots'
+    name = 'apps.screenshots'  # ← CORRIGIDO: com o prefixo 'apps.'
     verbose_name = 'Screenshots'
